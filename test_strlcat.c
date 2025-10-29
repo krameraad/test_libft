08f21500a71ebe830d563b3b34df42f00df5bb89
Elias Kramer <eliaspowder@gmail.com>
2025-10-29 16:21:02 +0100

    strlcat: new tests

--- test_strlcat.c
+++ test_strlcat.c
@@ -5,19 +5,33 @@
 #include <string.h>
 #include <bsd/string.h>
 
+#define YELLOW "\e[33m"
+#define DEFAULT "\e[39m"
+
+#define NORMAL "\e[0m"
+#define BOLD "\e[1m"
+#define DIM "\e[2m"
+#define HEADER "\e[94m\e[1;4m"
+#define TEST(x, y, z) printf(NORMAL "input:\t\t\"%s\", \"%s\", %zu\n", x, y, z); \
+strlcpy(str2, x, z); \
+printf(BOLD "got:\t\t%zu\n", ft_strlcat(str2, y, z)); \
+printf(NORMAL YELLOW "str: \"%s\"\n" DEFAULT, str2); \
+strlcpy(str1, x, z); \
+printf(DIM "expected:\t%zu\n", strlcat(str1, y, z)); \
+printf(NORMAL YELLOW "str: \"%s\"\n" DEFAULT, str1); \
+printf("\n");
+
 int	main(void)
 {
-	char	dest[] = "rrrrrr\0\0\0\0a\0\0a\0";
+	char	str1[100];
+	char	str2[100];
 
-	// dest = (char *)malloc(sizeof(*dest) * 15);
-	// ft_memset(dest, 0, 15);
-	// ft_memset(dest, 'r', 6);
-	// dest[11] = 'a';
-	// dest[0] = '\0';
-	// dest[11] = 'a';
-	// dest[14] = 'a';
-	printf("%s\n", dest);
-	printf("%zu\n", ft_strlcat(dest, "lorem ipsum dolor sit amet", 15));
-	printf("%s\n", dest);
+	printf(HEADER "## ft_strlcat\n\n");
+
+	TEST("lorem", "ipsum", 50UL);
+	TEST("lorem", "ipsum", 3UL);
+	TEST("lorem", "ipsum", 8UL);
+	TEST("lorem", "", 50UL);
+	TEST("", "", 50UL);
 	return (0);
 }
