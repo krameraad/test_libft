4b66f8de6f23bcaff0fe2e0f0e34e8c7ff78fcbd
Elias Kramer <eliaspowder@gmail.com>
2025-10-29 13:27:03 +0100

    test_atoi: predefined test cases

--- test_atoi.c
+++ test_atoi.c
@@ -5,16 +5,31 @@
 #include <string.h>
 #include <bsd/string.h>
 
-int main()
+#define NORMAL "\e[0m"
+#define BOLD "\e[1m"
+#define DIM "\e[2m"
+#define TEST(x) printf(NORMAL "input:\t\t%s\n", x); \
+printf(BOLD "got:\t\t%d\n", ft_atoi(x)); \
+printf(DIM "expected:\t%d\n", atoi(x)); \
+printf("\n");
+
+int	main(void)
 {
-	char *s = "  +123 ";
-	int n1;
-	int n2;
-
-	n1 = atoi(s);
-	n2 = ft_atoi(s);
-
-	printf("%d\n", n1);
-	printf("%d", n2);
+	TEST("0");
+	TEST("1");
+	TEST("b 5");
+	TEST("   42   ");
+	TEST(" 20-3  ");
+	TEST("+679");
+	TEST("-1");
+	TEST("-+5");
+	TEST("+-42");
+	TEST("-2a03");
+	TEST("-679");
+	TEST(" \t2147483647");
+	TEST("\f-2147483648  ");
+	TEST(" -2147483648  ");
+	TEST(" -9999999999  ");
+	TEST(" 99999999999");
 	return (0);
 }
